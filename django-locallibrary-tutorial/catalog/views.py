479aa967bd824f44325eb75db7c41759f6fabcf6
Martin <martin.garciai@estudiante.uam.es>
2026-02-14 17:12:16 +0100

    testing

--- django-locallibrary-tutorial/catalog/views.py
+++ django-locallibrary-tutorial/catalog/views.py
@@ -52,14 +52,14 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 2
+    paginate_by = 10
 
 class BookDetailView(generic.DetailView):
     model = Book
 
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 2    
+    paginate_by = 10    
 
 class AuthorDetailView(generic.DetailView):
     model = Author
