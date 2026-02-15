f615bbcfbdcca006e6b1d0638359ec88f680f716
martin-ggg <martin.garciai@estudiante.uam.es>
2026-02-15 17:55:58 +0100

    Modify build script to use createsu.py for superuser
    Removed superuser creation from build script and replaced it with a shell command to run a separate script.

--- django-locallibrary-tutorial/build.sh
+++ django-locallibrary-tutorial/build.sh
@@ -6,8 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-export DJANGO_SUPERUSER_USERNAME=alumnodb
-export DJANGO_SUPERUSER_PASSWORD=alumnodb
-export DJANGO_SUPERUSER_EMAIL=admin@example.com
-python manage.py createsuperuser --no-input
+python manage.py shell < createsu.py
 python populate_catalog2.py
